f4fa805e90286e6f92ae4ff56509a47bff281619
Ilmir <ilmirbyashar@gmail.com>
2022-03-04 19:48:10 +0300

    добавление функций

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function Random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log (Random(0, 11));
+
+function Check(lineCheck, maxLength) {
+  if lineCheck == maxLength true else lineCheck != maxLength false;
+  console.log (Check(0, 11))
+  return Check(10, 11);
+}
