32d51541b65d5b594d2466d81ce710fd66427f64
Ilmir <ilmirbyashar@gmail.com>
2022-03-11 18:47:16 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -3,8 +3,14 @@ function Random(min, max) {
 }
 console.log (Random(0, 11));
 
+// function Check(lineCheck, maxLength) {
+//   if (lineCheck == maxLength)  {return Check(10, 11)} true else {} false;
+//   console.log (Check)
+//   // return Check(10, 11);
+// }
 function Check(lineCheck, maxLength) {
-  if lineCheck == maxLength true else lineCheck != maxLength false;
-  console.log (Check(0, 11))
-  return Check(10, 11);
+  if (lineCheck <= maxLength)  {return true;}
+  else {return false;}
+
 }
+Check(141, 140);
